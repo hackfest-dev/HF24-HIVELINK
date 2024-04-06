6527294c663c74c1a078f34404a14e640b212438
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-04-06 11:26:01 +0530

    data convertion

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,20 +1,28 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from ultralytics import YOLO
+import datetime
+import pytz
 
 
 app = FastAPI()
-
 model = YOLO("../ml_model/best.pt")
 
 
-
+def unix_to_datetime(unix_timestamp):
+    utc_datetime = datetime.datetime.fromtimestamp(unix_timestamp)
+    indian_timezone = pytz.timezone('Asia/Kolkata')
+    return pytz.utc.localize(utc_datetime).astimezone(indian_timezone)
 
 
 @app.post("/hive")
 async def hive(data: dict):
-    print(data)
-    return { "data": data }
+    humidity = data["humidity"]
+    temperature = data["temperature"]
+    audioURL = data["audioURL"]
+    dateTime = unix_to_datetime(data["dateTime"])
+    return {"data": dateTime}
+
 
 @app.get("/")
 def read_root():
